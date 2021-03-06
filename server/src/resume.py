4ce6bff867932b68a9d0d5a74740cf6ee41bde6c
andrew hernandez <anh190@ucsd.edu>
2021-03-06 15:30:44 -0800

    started

--- server/src/resume.py
+++ server/src/resume.py
@@ -1,25 +1,19 @@
 from flask import Flask
 from flask import Response, request, redirect, render_template
+
 import requests
 
 
 app = Flask(__name__)
 
-# dictionary to act as local cache storing all sites visited in a single instance of the program
-
-
-
-
-
-
 @app.route('/')
 def index():
-    render_template('home.html')
     
+    return render_template('home.html')
 
-@app.route('/Personal',methods=['GET'])
+@app.route('/Personal')
 def proxy():
-    render_template('home.html')
+    return render_template('home.html')
     
     
 
