b92913bd07bfb65008436b1ecfdcd0eb99ce2f3c
andrew hernandez <anh190@ucsd.edu>
2021-03-14 20:51:57 -0700

    done

--- server/src/resume.py
+++ server/src/resume.py
@@ -6,19 +6,30 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def index():
-    
+def personal():
     return render_template('home.html')
 
-@app.route('/Personal')
-def proxy():
-    return render_template('home.html')
-    
+@app.route('/Resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/Contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/git_repo')
+def toggle():
+    resp = requests.get("https://github.com/Toggle-development/TogDev2.0")
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
+    response = Response(resp.content, resp.status_code, headers)
+    return response
     
 
 
 if __name__ == '__main__':
-    app.run(debug = False,port=5000)
+    app.run(debug = True,port=5000)
 
     
